d26c5df333421baba7847a0abcd8948b8127a7d3
VladimirGladky <vovagl23@mail.ru>
2026-02-10 23:06:47 +0300

    feat: add repository layer

---
+++ internal/repository/repository.go
@@ -0,0 +1,133 @@
+package repository
+
+import (
+	"DelayedNotifier/internal/models"
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/wb-go/wbf/dbpg"
+	"github.com/wb-go/wbf/zlog"
+)
+
+type NotificationRepository struct {
+	ctx context.Context
+	db  *dbpg.DB
+}
+
+func NewNotificationRepository(ctx context.Context, db *dbpg.DB) *NotificationRepository {
+	return &NotificationRepository{
+		ctx: ctx,
+		db:  db,
+	}
+}
+
+func (r *NotificationRepository) CreateNotification(notification *models.Notification) error {
+	query := `
+		INSERT INTO notifications (id, message, time, status, chat_id)
+		VALUES ($1, $2, $3, $4, $5)
+	`
+
+	_, err := r.db.ExecContext(
+		r.ctx,
+		query,
+		notification.Id,
+		notification.Message,
+		notification.Time,
+		notification.Status,
+		notification.ChatId,
+	)
+	if err != nil {
+		zlog.Logger.Error().
+			Err(err).
+			Str("notification_id", notification.Id).
+			Msg("Failed to create notification in DB")
+		return err
+	}
+	zlog.Logger.Info().
+		Str("notification_id", notification.Id).
+		Msg("Notification created in DB")
+	return nil
+}
+
+func (r *NotificationRepository) GetNotification(id string) (*models.Notification, error) {
+	query := `
+  		SELECT id, message, time, status, chat_id
+  		FROM notifications
+  		WHERE id = $1
+  	`
+
+	notification := &models.Notification{}
+
+	err := r.db.QueryRowContext(r.ctx, query, id).Scan(
+		&notification.Id,
+		&notification.Message,
+		&notification.Time,
+		&notification.Status,
+		&notification.ChatId,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("notification not found: %s", id)
+		}
+		zlog.Logger.Error().
+			Err(err).
+			Str("notification_id", id).
+			Msg("Failed to get notification from DB")
+		return nil, fmt.Errorf("failed to get notification: %w", err)
+	}
+
+	return notification, nil
+}
+
+func (r *NotificationRepository) UpdateNotificationStatus(id string, status string) error {
+	query := `
+  		UPDATE notifications
+  		SET status = $1
+  		WHERE id = $2
+  	`
+	_, err := r.db.ExecContext(r.ctx, query, status, id)
+	if err != nil {
+		zlog.Logger.Error().
+			Err(err).
+			Str("notification_id", id).
+			Str("status", status).
+			Msg("Failed to update notification status")
+		return fmt.Errorf("failed to update notification status: %w", err)
+	}
+
+	zlog.Logger.Debug().
+		Str("notification_id", id).
+		Str("status", status).
+		Msg("Notification status updated")
+	return nil
+}
+
+func (r *NotificationRepository) DeleteNotification(id string) error {
+	query := `
+  		UPDATE notifications
+  		SET status = 'cancelled'
+  		WHERE id = $1
+  	`
+
+	result, err := r.db.ExecContext(r.ctx, query, id)
+	if err != nil {
+		zlog.Logger.Error().
+			Err(err).
+			Str("notification_id", id).
+			Msg("Failed to delete notification")
+		return fmt.Errorf("failed to delete notification: %w", err)
+	}
+
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		return fmt.Errorf("notification not found: %s", id)
+	}
+
+	zlog.Logger.Info().
+		Str("notification_id", id).
+		Msg("Notification cancelled")
+	return nil
+}
